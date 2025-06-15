5fd740c91d1a02f3d82f7569b030061ce6203ee0
noa-keter <noaketer123@gmail.com>
2025-06-15 18:28:09 +0300

    [simp_helpers] header file + write to reg func
    implement write_registers_to_file() function and create a header file to the simp_helpers

--- simulator/sim_helpers.c
+++ simulator/sim_helpers.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
+//#include "register.h" //uncomment before merge
+#include "register_delete_before_merge.h" //before merge change it to "register.h"
 
 int write_str_to_file(char* output_path, char* str) {
 	// This function writes a string to a file specified by output_path.
@@ -33,6 +35,28 @@ int write_num_to_file(char* output_path, int num) {
 	if (fprintf(file, "%08X", num) < 0) {
 		fclose(file);
 		return -1;
+	}
+
+	fclose(file);
+	return 0;
+}
+
+int write_registers_to_file(char* regout_path, int32_t* reg_array) {
+	// This function writes the values of registers R2 - R15 to a file.
+	int num_regs = REG_NUM; // R2 to R15 inclusive
+	FILE* file = fopen(regout_path, "w");
+
+
+	// Check if file opens successfully
+	if (file == NULL)
+		return -1;
+
+	// Write R2 to R15
+	for (int i = 2; i < num_regs; i++) {
+		if (fprintf(file, "%08X\n", reg_array[i]) < 0) {
+			fclose(file);
+			return -1;
+		}
 	}
 
 	fclose(file);
---
+++ simulator/simp_helpers.h
@@ -0,0 +1,9 @@
+#include <stdint.h>
+#include <stdlib.h>
+
+/* Write functions */
+int write_str_to_file(char* output_path, char* str);
+int write_num_to_file(char* output_path, int num);
+int write_registers_to_file(char* regout_path, int32_t* reg_array);
+
+
