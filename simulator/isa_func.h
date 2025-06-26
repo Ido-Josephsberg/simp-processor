9c29923878233ae28e4b519acb06d560e7015b51
Ido-Josephsberg <idoj10@gmail.com>
2025-06-26 12:02:41 +0300

    [simulator][isa_funcs] add isa_opcode enum

--- simulator/isa_func.h
+++ simulator/isa_func.h
@@ -1,1 +1,27 @@
 #pragma once
+
+
+typedef enum {
+	ADD, // 0
+	SUB, // 1
+	MUL, // 2
+	AND, // 3
+	OR, // 4
+	XOR, // 5
+	SLL, // 6
+	SRA, // 7
+	SRL, // 8
+	BEQ, // 9
+	BNE, // 10
+	BLT, // 11
+	BGT, // 12
+	BLE, // 13
+	BGE, // 14
+	JAL, // 15
+	LW, // 16
+	SW, // 17
+	RETI, // 18
+	IN, // 19
+	OUT, // 20
+	HALT // 21
+}isa_opcode;
