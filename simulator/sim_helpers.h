43b53040dcf05ec859caae991e99251cb670f3f4
Ido-Josephsberg <idoj10@gmail.com>
2025-06-26 12:08:48 +0300

    define params and macro funcs which are in general use

---
+++ simulator/sim_helpers.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "register.h"
+
+
+
+#define CYCLE_NUM_OF_DIGITS 8
+#define PC_NUM_OF_DIGITS 3
+#define INST_NUM_OF_DIGITS 8
+#define MACRO_TO_STR(x) STR(x) // Interpret the macro and convert it to a string literal
+#define STR(x) #x // Converts x to a string literal
+
+
