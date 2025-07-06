a8af718640ecbc489636d4cd020aa8798718c9db
OFKBR <ofekbaruch1@mail.tau.ac.il>
2025-07-06 20:37:22 +0300

    asm program files (3/4)

---
+++ programs/factorial.asm
@@ -0,0 +1,31 @@
+init:
+    add $sp, $sp, $imm, -3              # adjust stack for 3 items
+    sw $a0, $sp, $imm, 0                # save initial argument on stack_0[0]
+    sw $ra, $sp, $imm, 1                # save initial return address on stack_0[1]
+    sw $v0, $sp, $imm, 2                # save initial return value on stack_0[2]
+    lw $a0, $zero, $imm, 0x100          # $a0 = n
+    lw $ra, $zero, $imm, finalize       # $ra = address of finalize
+factorial:
+    add $sp, $sp, $imm, -2              # adjust stack for 2 items
+    sw $a0, $sp, $imm, 0                # save n on stack_i[0]
+    sw $ra, $sp, $imm, 1                # save ra on stack_i[1]
+    beq $a0, $zero, $imm, if            # if n == 0, enter if block
+else:
+    sub $a0, $a0, $imm, 1               # $a0 = n-1
+    jal $ra, $imm, zero, factorial      # recursive call: factorial(n-1)
+    lw $a0, $sp, $imm, 0                # restore argument n
+    mul $v0, $a0, $v0                   # $v0 = n * factorial(n-1)
+    beq $imm, $zero, $zero, done        # jump to done
+if:
+    add $v0, $zero, $imm, 1             # return 1 (base case)
+done:
+    lw $ra, $sp, $imm, 1                # restore return address
+    add $sp, $sp, $imm, 2               # pop 2 items from stack
+    beq $ra, $zero, $zero, 0            # return to caller
+finalize:
+    sw $v0, $zero, $imm, 0x101          # store result in 0x101
+    lw $a0, $sp, $imm, 0                # restore initaial argument from stack_0[0]
+    lw $ra, $sp, $imm, 1                # restore initaial return address from stack_0[1]
+    lw $v0, $sp, $imm, 2                # restore initaial return value from stack_0[2]
+    add $sp, $sp, $imm, 3               # pop 3 items from stack
+    halt $zero, $zero, $zero, 0         # halt the program
---
+++ programs/sort.asm
@@ -0,0 +1,35 @@
+init: 
+    add $sp, $sp, $imm, -3	    	# adjust stack for 3 items
+    sw $s0, $sp, $imm, 0            # save $s0 on stack[0]
+    sw $s1, $sp, $imm, 1            # save $s1 on stack[1] 
+    sw $s2, $sp, $imm, 2            # save $s2 on stack[2]
+    add $s0, $zero, $imm, 0x100     # $s0 = base address of the array
+    add $s1, $zero, $zero, 0        # $s1 = i = 0
+for_i:
+    add $t0, zero, $imm, 16         # $t0 = 16 (break limit for i)
+    beq $s1, $t0, $imm, done        # if i==16, break loop (finish)
+    add $s2, $zero, $zero, 0        # $s2 = j = 0
+for_j:
+    sub $t0, $imm, $t0, 15          # $t0 = 15-i (break limit for j)
+    beq $s2, $t0, $imm, inc_i       # if j == 15-i, break loop (next i)
+    lw $t0, $s0, $s2, 0             # $t0 = arr[j]
+    add $t1, $s2, $imm, 1           # $t1 = j+1
+    lw $t1, $s0, $t1, 0             # $t1 = arr[j+1]
+    ble $imm, $t0, $t1, inc_j       # if arr[i] <= arr[j+1], dont swap
+swap:
+    add $t2, $t0, $zero, 0          # $t2 = temp copy of arr[j]
+    add $t0, $s0, $s2, 0            # $t0 = address arr[j]
+    sw $t1, $t0, $zero, 0           # arr[j] = arr[j+1]
+    sw $t2, $t0, $imm, 1            # arr[j+1] = temp
+inc_j:
+    add $s2, $s2, $imm, 1           # j++
+    beq $imm, $zero, $zero, for_j   # jump to for_j    
+inc_i:
+    add $s1, $s1, $imm, 1           # i++
+    beq $imm, $zero, $zero, for_i   # jump to for_i
+done:
+    lw $s0, $sp, $imm, 0            # restore $s0 from stack[0]
+    lw $s1, $sp, $imm, 1            # restore $s1 from stack[1]
+    lw $s2, $sp, $imm, 2            # restore $s2 from stack[2]
+    add $sp, $sp, $imm, 3           # pop 3 items from stack
+    halt $zero, $zero, $zero, 0     # halt the program
